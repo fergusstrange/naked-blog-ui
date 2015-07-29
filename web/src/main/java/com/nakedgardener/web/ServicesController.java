b284ff7dc92b4644da89ac3043db1a33810f58ea
Fergus Strange <fergusstrange@gmail.com>
2015-07-29 13:04:42 +0100

    Rename to services controller.

--- web/src/main/java/com/nakedgardener/web/ServicesController.java
+++ web/src/main/java/com/nakedgardener/web/ServicesController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomepageController {
+public class ServicesController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String homepage() {
+    public String services() {
         return "services";
     }
 }
