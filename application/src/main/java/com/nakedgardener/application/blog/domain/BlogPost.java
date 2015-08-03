0415f1a003282803593e9539f4f29f0d9a734cd6
Fergus Strange <fergusstrange@gmail.com>
2015-08-03 20:14:04 +0100

    Adding lombok stuff.

--- application/src/main/java/com/nakedgardener/application/blog/domain/BlogPost.java
+++ application/src/main/java/com/nakedgardener/application/blog/domain/BlogPost.java
@@ -2,11 +2,17 @@ package com.nakedgardener.application.blog.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BlogPost {
 
