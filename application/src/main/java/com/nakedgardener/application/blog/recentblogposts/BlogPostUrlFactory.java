cc00379ec40fd685719bc6c46df8d312a56a5a50
Fergus Strange <fergus.strange@infinityworks.com>
2015-10-31 17:29:09 +0000

    Change to use uri builder.

--- application/src/main/java/com/nakedgardener/application/blog/recentblogposts/BlogPostUrlFactory.java
+++ application/src/main/java/com/nakedgardener/application/blog/recentblogposts/BlogPostUrlFactory.java
@@ -1,11 +1,15 @@
 package com.nakedgardener.application.blog.recentblogposts;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class BlogPostUrlFactory {
 
     public String blogPostURL(String blogPostSlug) {
-        return String.format("/blog/%s", blogPostSlug);
+        return UriComponentsBuilder
+                .fromPath("/blog")
+                .pathSegment(blogPostSlug)
+                .toUriString();
     }
 }
