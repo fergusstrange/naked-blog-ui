99a63f08d75db039876f814beda62bf721066272
Fergus Strange <fergus.strange@infinityworks.com>
2015-11-09 09:29:01 +0000

    Unused code.

--- application/src/main/java/com/nakedgardener/application/blog/BlogRestServiceURLFactory.java
+++ application/src/main/java/com/nakedgardener/application/blog/BlogRestServiceURLFactory.java
@@ -12,22 +12,10 @@ import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
 public class BlogRestServiceURLFactory {
 
     private final String baseBlogURL;
-    private final int blogChunkSize;
 
     @Autowired
-    public BlogRestServiceURLFactory(@Value("${blog.base.url:http://localhost:8888}") String baseBlogURL,
-                                     @Value("${blog.chunk.size:5}") int blogChunkSize) {
+    public BlogRestServiceURLFactory(@Value("${blog.base.url:http://localhost:8888}") String baseBlogURL) {
         this.baseBlogURL = baseBlogURL;
-        this.blogChunkSize = blogChunkSize;
-    }
-
-    public URI mostRecentBlogPostsURL(int page) {
-        return fromHttpUrl(baseBlogURL)
-                .path("/blog-post/_recent")
-                .queryParam("page", page)
-                .queryParam("pageSize", blogChunkSize)
-                .build()
-                .toUri();
     }
 
     public URI blogPostURL(String blogPostSlug) {
